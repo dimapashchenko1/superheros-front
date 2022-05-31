35f4cbf1f1cf4387a34a4edc83e495a42eac95f8
dimapashchenko1 <76619935+dimapashchenko1@users.noreply.github.com>
2022-05-31 03:28:24 +0300

    api heroku

--- src/api/settings.js
+++ src/api/settings.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export default axios.create({
-  // baseURL: "https://superhero-project.herokuapp.com/api/heros/",
-  baseURL: "http://localhost:3333/api/heros/",
+  baseURL: "https://superheros-app-back.herokuapp.com/api/heros/",
+  // baseURL: "http://localhost:3333/api/heros/",
   headers: {
     "Content-type": "application/json"
   }
